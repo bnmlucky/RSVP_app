aae8d85625a1a241b1932c01843907548c54b8c1
Natalia Titova <bnmlucky@gmail.com>
2020-05-30 10:36:50 -0400

    created an edit button in the delete event handler

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = input.value;
-    input.value = '';
+function createLI(text) {
     const li = document.createElement('li');
     li.textContent = text;
     const label = document.createElement('label');
@@ -14,6 +11,22 @@ form.addEventListener('submit', (e) => {
     checkbox.type = 'checkbox';
     label.appendChild(checkbox);
     li.appendChild(label);
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'edit';
+    li.appendChild(editButton);
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    li.appendChild(removeButton);
+    return li;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const li = createLI(text);
     ul.appendChild(li);
 });
 
@@ -25,5 +38,18 @@ ul.addEventListener('change', (e) => {
         listItem.className = 'responded';
     } else {
         listItem.className = '';
+    }
+});
+
+ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const button = e.target;
+        const li = button.parentNode;
+        const ul = li.parentNode;
+        if (button.textConent === 'remove') {
+            ul.removeChild(li);
+        } else if (button.textContent === 'edit') {
+            console.log('edit');
+        }
     }
 });
